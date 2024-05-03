2bebb915d4fc6d2e3402d84fe23692ccf071b34e
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-05-03 22:02:47 +1000

    add Fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,8 @@
-print("Hello, world!!!")
+from fastapi import FastAPI
+
+from gigachat import GigaChat_Client
+from config import BASE_URL
+from router import router as routers
+
+app = FastAPI()
+app.include_router(routers)
